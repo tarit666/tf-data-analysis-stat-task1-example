0c4ecf6596f6029d3e5158065c70d45e240f2153
tarit666 <106433015+tarit666@users.noreply.github.com>
2023-04-06 14:40:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 681730925 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    # Не меняйте название функции и её аргументы v = s/t a = v/t 
+    for element in x:
+      x = x/51**2
+    grade = len(x)/sum(x)
+    return grade # Ваш ответ
